17a10f0d93244f626d09dbfffce4060ba9888a1d
stuwok <stuwokinfo@gmail.com>
2020-08-29 14:46:21 +0100

    Widgets Components

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import './Widgets.css'
+import React from 'react';
+import './Widgets.css';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from 'react-twitter-embed';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Widgets = () => {
-    return (
-        <div className="widgets">
-            <h2>Widgets</h2>
-        </div>
-    )
-}
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
 
-export default Widgets
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={'1298701285630398465'} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="davak_01"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton url={'https://facebook.com/david.akpughe'} />
+      </div>
+    </div>
+  );
+};
+
+export default Widgets;
